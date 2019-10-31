87909d62b24a7531e9d7f3e3771a6ec3cfbf7e37
Matt Sadowski <github@yourmatt.com>
2019-10-31 08:27:35 -0700

    Set feed time area to use state manager

--- frontend-src/js/components/LastFeedTime.jsx
+++ frontend-src/js/components/LastFeedTime.jsx
@@ -4,15 +4,33 @@
 
 import React, {Component} from "react";
 
+// import utilities
 import FormatCssClass from "../utils/FormatCssClass.jsx";
+import StateManager from "../utils/StateManager.jsx";
 
 export default class LastFeedTime extends Component {
 
+    // Constructor.
+    constructor(props, context) {
+        super(props, context);
+        this.previousState = {};
+
+        StateManager.Store.subscribe(() => {
+            if (StateManager.ValueChanged(this.previousState, [
+                    "SelectedBaby",
+                    "Babies.Baby" + StateManager.State().SelectedBaby + ".LastFeedTime"
+                ]
+            )) this.forceUpdate();
+        });
+
+    }
+
+    // Renders the last feed time area.
     render() {
 
         return (
             <div className={FormatCssClass("last-feed")}>
-                Last Fed at {this.props.lastFeedTime}
+                Last Fed at {StateManager.GetCurrentBabyDetails().LastFeedTime}
             </div>
         );
 
